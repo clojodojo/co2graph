99f65f73a7e2d0d03f79f564b71281eb482e65c3
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-08-04 22:44:38 +0000

    Show date lines and fan lines

--- src/demo/ui.cljs
+++ src/demo/ui.cljs
@@ -4,6 +4,7 @@
     [reagent.core :as r]
     [tick.core :as t]
     [goog.labs.format.csv :as csv]
+    [tick.timezone]
     [tick.locale-en-us]))
 
 (defonce co2-data (r/atom []))
@@ -21,30 +22,67 @@
                           data)))
       (.then (fn [data]
               (map (fn [[date co2 _temperature _humidity _pressure]]
-                    [(t/<< (cljc.java-time.local-date-time/parse date formatter)
-                           (t/new-duration 4 :hours)) co2])
+                    [(cljc.java-time.local-date-time/parse date formatter) co2])
                    (rest data))))
       (.then (fn [data] (reset! co2-data data))))
 
   (-> (js/fetch "/log.edn")
       (.then (fn [e] (.text e)))
       (.then (fn [text] (cljs.reader/read-string text)))
+      ;; TODO fix the data points to be in toronto time
+      #_(t/<< (cljc.java-time.local-date-time/parse date formatter)
+              (t/new-duration 4 :hours))
       (.then (fn [data] (reset! event-data data)))))
 
 
 (defn date-range [start end step]
-  ;; find first "midhnight" after start
   (loop [dates (list start)]
     (let [date (first dates)]
      (if (< date end)
        (recur (conj dates (t/>> date step)))
        (reverse dates)))))
 
+
+
+;; find first "midnight" after start
+
 #_(date-range (t/now)
               (t/>> (t/now) (t/new-duration 10 :days))
               (t/new-duration 1 :days))
-
+#_(t/at (t/date (t/now)) (t/midnight))
 #_(t/>> (t/now) (t/new-duration 1 :days))
+
+#_(t/in (t/now) "America/Toronto")
+
+
+;;  [[1  true]
+;;    ....
+;;   [6  false]
+;;   ....
+;;   [8  true]
+;;    ....
+;;   [10  false]]
+
+;; ???
+
+;; [[1 6] [8 10]]
+;;
+;; line 1->6,  line 8->10
+
+
+(defn on-states [data f]
+ (reduce (fn [accum record]
+            (case (f record)
+              nil accum
+              true (conj accum [(first record)])
+              false (update accum (dec (count accum)) conj (first record))))
+         [] data))
+
+#_(on-states [[1 true] [2 nil] [4 nil] [6 false] [7 nil] [8 true] [9 nil] [10 false] [11 nil]]
+             second)
+;; [[1 6] [8 10]]
+
+
 
 (defn app-view []
   [:div
@@ -85,9 +123,38 @@
                    :y1 (->y co2)
                    :y2 (->y co2)}])
          ;; color bands
-         [band 0 420 "#00FF0044"]
-         [band 1000 max-co2 "#FF000044"]
-         [band 420 550 "#0000FF44"]
+         #_[band 0 420 "#00FF0044"]
+         #_[band 1000 max-co2 "#FF000044"]
+         #_[band 420 550 "#0000FF44"]
+         ;; date lines
+         (for [date (date-range (t/at (t/date start-date) (t/midnight))
+                                (t/>> (t/now) (t/new-duration 5 :days))
+                                (t/new-duration 1 :days))]
+              ^{:key date}
+              [:line {:stroke "black"
+                      :x1 (->x (t/seconds (t/between start-date date)))
+                      :x2 (->x (t/seconds (t/between start-date date)))
+                      :y1 (->y 0)
+                      :y2 (->y max-co2)}])
+         ;; oven fan lines
+         (for [[start end] (on-states @event-data (fn [event] (get event 2)))]
+          ^{:key [start end]}
+          [:line
+           {:stroke "#ff000088"
+            :stroke-width 1000
+            :x1 (->x (t/seconds (t/between start-date start)))
+            :x2 (->x (t/seconds (t/between start-date end)))
+            :y1 (->y 500)
+            :y2 (->y 500)}])
+         (for [[start end] (on-states @event-data (fn [event] (get event 3)))]
+          ^{:key [start end]}
+          [:line
+           {:stroke "#00ff0033"
+            :stroke-width 1000
+            :x1 (->x (t/seconds (t/between start-date start)))
+            :x2 (->x (t/seconds (t/between start-date end)))
+            :y1 (->y 500)
+            :y2 (->y 500)}])
          ;; events
          #_(for [[date label] @event-data]
             ^{:key date}
